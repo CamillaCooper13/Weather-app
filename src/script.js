e45632726acd1194eb03388f9538b070abdd1b54
CamillaCooper13 <71074056+CamillaCooper13@users.noreply.github.com>
2020-11-19 15:55:21 +0000

    Added search engine, current button and fixed temp error

--- src/script.js
+++ src/script.js
@@ -7,7 +7,8 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-    let dayIndex = date.getDay();
+
+  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -22,31 +23,67 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayTemperature(response){
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let precipitationElement = document.querySelector("#precipitation");
-  let dateElement = document.querySelector("#date");
-let iconElement = document.querySelector("#icon");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  precipitationElement.innerHTML = response.data.precipitation;
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute = ("src", `https://ssl.gstatic.com/onebox/weather/64/${response.data.weather[0].icon}sunny.png`
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
-let apiKey  = "8c03abbdd5f78bfba8c1df25fce030aa";
-let city = "Paris"
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?=Singapore&appid=${apiKey}&units=metric`;
+
+function searchCity(city) {
+  let apiKey = "8c03abbdd5f78bfba8c1df25fce030aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "8c03abbdd5f78bfba8c1df25fce030aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+    position.coords.latitude
+  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
 
 
-
-axios.get(apiUrl).then(displayTemperature);
